b3ebe957b96a4fcac39f7789311534cbf4f66d7a
RyanMSaunders <ryan.s.maguire@gmail.com>
2025-02-06 21:40:18 -0500

    update music path in media player

--- source/javascripts/media-player.js
+++ source/javascripts/media-player.js
@@ -16,7 +16,7 @@ const music_list = [
     {
         name : 'Space Baby',
         artist : 'Ryan Maguire',
-        music : './music/Space-Baby.mp3'
+        music : 'https://ryanmsaunders.github.io/music-portfolio/music/Space-Baby.mp3'
     }
 ];
 
