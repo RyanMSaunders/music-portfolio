c48f4940588e9eed1333eaba3a80f7bd2492898f
RyanMSaunders <ryan.s.maguire@gmail.com>
2025-02-06 21:16:35 -0500

    add music folder to root

--- source/javascripts/media-player.js
+++ source/javascripts/media-player.js
@@ -16,7 +16,7 @@ const music_list = [
     {
         name : 'Space Baby',
         artist : 'Ryan Maguire',
-        music : './build/music/Space-Baby.mp3'
+        music : './music/Space-Baby.mp3'
     }
 ];
 
